40cc2537ea3e633b734993984f5a95604978b980
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-03-16 09:46:20 +0900

    js

---
+++ 1. 자바스크립트 기초/9. 중첩반복문.js
@@ -0,0 +1,11 @@
+// 구구단 3단 출력
+
+// var level = +prompt(`단을 입력하세요.`);
+
+
+for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= 9; j++) {
+        console.log(`${i} x ${j} = ${i*j} `);
+    }
+   console.log(`=======================`); 
+}
