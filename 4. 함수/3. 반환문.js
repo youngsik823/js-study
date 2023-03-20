8bd84b05a5a5a695466008657727529f789bfb0e
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-03-20 16:17:17 +0900

    js

--- 4. 함수/3. 반환문.js
+++ 4. 함수/3. 반환문.js
@@ -26,3 +26,31 @@ multi(10, 3);
 
 var r3 = multi(10, 3);
 // 리턴값이 없어서 r3에 저장이 안된다 undefined
+
+// return 탈출문
+function callName(nickName) {
+    var prohibits = ['멍청이', '돌아이', '홍길동'];
+
+    if (prohibits.includes(nickName)) {
+        console.log(`잘못된 닉네임은 인사를 못해줘 이놈아`);
+        return; // 강제로 끝냄
+    }
+    console.log(`${nickName}님 안녕하세요!`);
+}
+callName(`돌아이`);
+
+function stopLoop() {
+
+    while (true) {
+        var flag = prompt('명령어를 입력하세요!');
+        if (flag === '멈춰') {
+            break;
+        }
+        alert(flag);
+    }
+    alert('수고용~');
+}
+stopLoop();
+
+// break는 while문을 종료해 주지만
+// return은 함수 자체를 종료시켜준다
