ce53e6bd228d58545bd4ba944b01650189ba8b0e
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-03-20 10:43:21 +0900

    js

---
+++ 3. 객체/1. 객체 기초.js
@@ -0,0 +1,159 @@
+// 객체 리터럴
+var dog = {
+    // 프로퍼티 작성   
+    'name': '뽀삐',
+    kind: '진돗개',
+    age: 3,
+    // 'for waiting' : 11,
+    // 문자어를 만들면 띄어쓰기가 가능하다.
+    injection: true,
+    favorite:['산책', '간식'],
+    hate: null,
+
+    playWithChild: function(){}
+    // 함수도 넣을 수 있다.
+};
+// 프로퍼티는 예약어를 쓸 수 있다.
+
+var cat = {
+    // 프로퍼티 작성   
+    'name': '콩순이',
+    kind: '블랙러시안',
+    age: 2,
+    injection: true,
+    favorite:['낮잠자기', '방어지럽히기'],
+    // name, kind 같은거는 변수가 아니라 프로퍼디 이다.
+};
+
+console.log(typeof cat);
+
+// 객체에 저장된 데이터 참조
+console.log(dog.name);
+// .은 ~의 라고 해석
+// 개의 이름
+console.log(cat.age);
+console.log(cat.favorite);
+console.log(dog.favorite[0]);
+
+dog.favorite.push('꼬리 흔들기');
+console.log(dog);
+
+dog.favorite.slice(1).splice(1, 1);
+
+// 프로퍼티 참조 2
+console.log(`======================`);
+console.log(dog.injection);
+console.log(dog[`injection`]);
+
+// key를 변수에 저장
+// 이거 햇갈리기 때문에 잘 알아야 한다.
+var fv = 'favorite';
+console.log(cat[fv]);
+
+// 프로퍼티 값 수정
+dog.age = 4;
+cat.favorite[1] = '실뭉치';
+
+dog['injection'] = false;
+console.log(dog);
+
+// 프로퍼티 동적 추가
+cat.owner = `김철수`;
+console.log(cat);
+
+// 프로퍼티 삭제
+delete cat.owner;
+console.log(cat);
+
+console.log('==========================');
+
+// 프로퍼티 존재 유무 확인
+// key를 반드시 문자열로 표기해야함.
+
+var ageFlag = 'age' in cat;
+console.log(ageFlag);
+
+// master라는 프로퍼티가 없으면 추가해라
+var m = 'master';
+if(!(m in cat)){
+    cat[m] = `김또또`;
+}
+console.log(cat);
+
+console.log('============================');
+
+// 객체를 순회하는 반복문
+// of면 뒤에 배열
+// in이면 뒤에 객체
+// key를 반복해서 추출해줌
+for (var k in dog) {
+    // console.log(typeof k); 
+    console.log(dog[k]);
+    // k가 String이기 때문에 'name' 처럼 따옴표를 달고 나온다
+    // 그러므로 dog.k를 하면 안된다. dog[k] 로 써야한다.
+}
+
+
+//객체의 중첩구조
+//예시 : 게시판
+
+var articles = {
+    totalCount: 25578,  //총 게시물 수
+    admin: 'abc1234', //게시판 관리자 아이디
+    page: 2558, //총 페이지 수
+    articleList: [ // 게시물 목록
+        {
+            bno: 3, //글번호
+            title: '가위바위보', //글제목
+            writer: '김짱껨뽀',  //작성자
+            content: '다덤벼 ^^', //글내용
+            viewCount: 53, //조회수
+            recomm: 10, //추천수
+            regDate: '21-12-07' //작성일자
+        }, 
+        {
+            bno: 2, //글번호
+            title: '노는게', //글제목
+            writer: '뽀로로',  //작성자
+            content: '제일조와~~~', //글내용
+            viewCount: 253, //조회수
+            recomm: 11, //추천수
+            regDate: '21-12-06' //작성일자
+        }, 
+        {
+            bno: 1, //글번호
+            title: '아멘', //글제목
+            writer: '개신교신자',  //작성자
+            content: '할렐루야', //글내용
+            viewCount: 23, //조회수
+            recomm: 5, //추천수
+            regDate: '21-12-05' //작성일자
+        }
+    ]
+};
+
+console.log(articles.totalCount);
+console.log(articles.articleList[1].writer);
+
+var bbs = articles.articleList;
+
+bbs[0].writer;
+
+for(var x in articles) {
+
+}
+// 4바퀴 돌음 (key가 4개 있으니까)
+
+for (var y of bbs) {
+
+}
+// 3바퀴 돌음 (배열의 길이만큼)
+
+
+
+
+
+
+
+
+
